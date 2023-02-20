bdcc56f57bcdb3b5f4e9853c9a3bd05b4ebd4008
ali segueg <alisegueg123@outllok.fr>
2023-02-20 15:53:35 +0100

    task4 args kwars

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,45 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+
+    """On va etuliser args et kwargs pour
+        la création d'une base d'un model"""
+    def __init__(self, *args, **kwargs):
+            """Dans la méthode __init__(), on vérifie si l'argument kwargs est présent, ce qui
+              indique que l'objet doit être créé à partir d'un dictionnaire. Si kwargs n'est pas vide, 
+              on parcour chaque paire clé-valeur dans le dictionnaire. Si la clé est created_at 
+              ou updated_at, vous convertissez la valeur de la chaîne de caractères en objet datetime. 
+              Ensuite, vous utilisez la méthode setattr() pour définir l'attribut de l'objet correspondant 
+              à la clé avec la valeur."""
+            if kwargs:
+                 for key, value in kwargs.items():
+                      if key == 'created_at' or key == 'updated_at':
+                           """convertie l'objet en datatime"""
+                           value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                      if key != '__class__':
+                           setattr(self, key, value)
+            if not kwargs:
+                
+                """id permettra d'avoir un identifiant unique transformer en str a chaque
+                instancation de class BaseModel"""
+                self.id = str(uuid.uuid4())
+
+                """attribuer la date et l'heure a la création de instance class BaseModel"""
+                self.created_at = datetime.now()
+
+                """Permet d'attribut la date et l'heure quand une instance"""
+                self.updated_at = self.created_at
         
     def __str__(self):
+        """Methode special qui return la chaine suivante"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
         
     def save(self):
+        """A chaque fois qu'il y aura une modification, l'attribut sera mise à jour"""
         self.updated_at = datetime.now()
         
     def to_dict(self):
+        """On va return un dictionner avec les noms (clées) et les valeur d'un attributs"""
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = self.created_at.isoformat()
--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -1,39 +1,63 @@
+import unittest
 from models.base_model import BaseModel
 from datetime import datetime
-import unittest
-import uuid
-
+import time
 
 class TestBaseModel(unittest.TestCase):
     
-    def test_id_is_uuid(self):
-        # Vérifie que l'id de l'objet est un UUID valide
-        model = BaseModel()
-        self.assertTrue(uuid.UUID(model.id))
-        
-    def test_created_and_updated_at_are_datetimes(self):
-        # Vérifie que les attributs created_at et updated_at sont des objets datetime valides
-        model = BaseModel()
-        self.assertIsInstance(model.created_at, datetime)
-        self.assertIsInstance(model.updated_at, datetime)
-        
-    """
+    def setUp(self):
+        self.model = BaseModel()
+
+    def test_has_id(self):
+        self.assertTrue(hasattr(self.model, 'id'))
+
+    def test_id_is_string(self):
+        self.assertIsInstance(self.model.id, str)
+
+    def test_has_created_at(self):
+        self.assertTrue(hasattr(self.model, 'created_at'))
+
+    def test_created_at_is_datetime(self):
+        self.assertIsInstance(self.model.created_at, datetime)
+
+    def test_has_updated_at(self):
+        self.assertTrue(hasattr(self.model, 'updated_at'))
+
+    def test_updated_at_is_datetime(self):
+        self.assertIsInstance(self.model.updated_at, datetime)
+
     def test_save_updates_updated_at(self):
-        # Vérifie que la méthode save met à jour l'attribut updated_at
-        model = BaseModel()
-        old_updated_at = model.updated_at
-        model.save()
-        self.assertGreater(model.updated_at, old_updated_at)
-    """    
+        old_updated_at = self.model.updated_at
+        time.sleep(0.0001)  # Attendre un peu
+        self.model.save()
+        self.assertGreater(self.model.updated_at, old_updated_at)
+
     def test_to_dict_returns_dict(self):
-        # Vérifie que la méthode to_dict renvoie un dictionnaire contenant les attributs de l'objet
-        model = BaseModel()
-        model_dict = model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertIn('id', model_dict)
-        self.assertIn('created_at', model_dict)
-        self.assertIn('updated_at', model_dict)
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-        
+        self.assertIsInstance(self.model.to_dict(), dict)
+
+    def test_to_dict_includes_class_name(self):
+        self.assertEqual(self.model.to_dict()['__class__'], 'BaseModel')
+
+    def test_to_dict_includes_created_at(self):
+        self.assertIn('created_at', self.model.to_dict())
+
+    def test_to_dict_includes_updated_at(self):
+        self.assertIn('updated_at', self.model.to_dict())
+
+    def test_to_dict_created_at_is_str(self):
+        self.assertIsInstance(self.model.to_dict()['created_at'], str)
+
+    def test_to_dict_updated_at_is_str(self):
+        self.assertIsInstance(self.model.to_dict()['updated_at'], str)
+
+    def test_str_returns_string(self):
+        self.assertIsInstance(str(self.model), str)
+
+    def test_str_includes_class_name(self):
+        self.assertIn('BaseModel', str(self.model))
+
+    def test_str_includes_id(self):
+        self.assertIn(self.model.id, str(self.model))
+
 if __name__ == '__main__':
     unittest.main()
