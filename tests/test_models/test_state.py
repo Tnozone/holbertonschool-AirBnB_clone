6e8e78b8aa9a4169c2a1fc01fdd17f76b0a10afa
ali segueg <alisegueg123@outllok.fr>
2023-02-21 19:48:09 +0100

    test

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,10 @@
+import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    def test_attributes(self):
+        state = State()
+        self.assertTrue(hasattr(state, "id"))
+        self.assertTrue(hasattr(state, "created_at"))
+        self.assertTrue(hasattr(state, "updated_at"))
+        self.assertTrue(hasattr(state, "name"))
