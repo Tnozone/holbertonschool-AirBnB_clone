91fb74bd9f345e9670a054a77a4fa3d83b735577
ali segueg <alisegueg123@outllok.fr>
2023-02-22 15:36:48 +0100

    mise a jour de la console pour les nouvelle heritage de la fonction base model

--- console.py
+++ console.py
@@ -1,7 +1,12 @@
 import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-import models
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.user import User
 
 
