b19c90645e558ba3524d8e70c90a12354d2184a8
ali segueg <alisegueg123@outllok.fr>
2023-02-22 15:04:42 +0100

    teste pour user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,15 @@
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+
+    def test_user_attributes(self):
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertTrue(hasattr(user, "password"))
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertTrue(hasattr(user, "last_name"))
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
