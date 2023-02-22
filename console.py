bce764c9b0c0aff190b32416c808852b06bfd071
ali segueg <alisegueg123@outllok.fr>
2023-02-22 16:41:14 +0100

    test valider

--- console.py
+++ console.py
@@ -30,8 +30,7 @@ class HBNBCommand(cmd.Cmd):
             if my_class[0] in HBNBCommand.list_class and \
                 my_func[0] in HBNBCommand.list_function:
                     arg = my_func[0] + ' ' + my_class[0] + ' ' + param[0]
-        return arg
-            
+        return arg     
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -72,16 +71,16 @@ class HBNBCommand(cmd.Cmd):
         if not arg:
             print("** class name missing **")
             return
-        
+
         class_name = args[0]
         if class_name not in self.tab:
             print("** class doesn't exist **")
             return
-        
+
         if len(args) < 2:
             print("** instance id missing **")
             return
-        
+
         id = args[1]
         key = "{}.{}".format(class_name, id)
         obj_all = models.storage.all()
@@ -120,27 +119,25 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, arg):
         """Prints all string representation of all instances based or not on the class name
         Usage: all [Class Name] or all"""
-    
+
         args = arg.split()
         obj_all = models.storage.all()
 
         # Si aucun argument n'est fourni, on affiche toutes les instances.
         if not len(args):
             print([str(obj_all[key]) for key in obj_all])
-        
+
         # Si un nom de classe est fourni, on affiche toutes les instances de cette classe.
         elif args[0] in self.tab:
             print([str(obj_all[key]) for key in obj_all if key.startswith(args[0] + ".")])
 
         else:
             print("** class doesn't exist **")
-
-               
+         
     def do_update(self, arg):
         """met à jour une instance en fonction du nom et de l'identifiant
-            en ajoutant ou en mettant à jour l'attribut"""
-        
-        
+            en ajoutant ou en mettant à jour l'attribut""" 
+
         args = arg.split()
         obj_all = models.storage.all()
 
@@ -165,7 +162,7 @@ class HBNBCommand(cmd.Cmd):
         if key not in obj_all:
             print("** no instance found **")
             return
-        
+
         # Vérification de la présence de l'attribut et de sa valeur.
         if len(args) == 2:
             print("** attribute name missing **")
@@ -173,7 +170,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 3:
             print("** value missing **")
             return
-        
+
         name = args[2]
         name_value = args[3]
 
